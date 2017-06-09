f5fa331a8740c0a422f4fd00263acacdcf5fb344
1027 <1027@hb1027>
2017-06-09 17:24:59 +0900

    SELECT

--- src/main/java/com/hanbit/db/DataControl.java
+++ src/main/java/com/hanbit/db/DataControl.java
@@ -2,7 +2,9 @@ package com.hanbit.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DataControl {
 
@@ -13,11 +15,25 @@ public class DataControl {
 		String password = "hanbit";
 		
 		Connection conn = null;
+		ResultSet resultSet = null;
 		
 		try {
 			conn = DriverManager.getConnection(url, user, password);
 			System.out.println("접속 성공");
 			
+			String sql = "SELECT no, name, age, addr FROM tbl_students";
+			
+			Statement stmt = conn.createStatement();
+			resultSet = stmt.executeQuery(sql);
+			
+			while (resultSet.next()) {
+				int no = resultSet.getInt("no");
+				String name = resultSet.getString("name");
+				int age = resultSet.getInt("age");
+				String addr = resultSet.getString("addr");
+				
+				System.out.println(no + "/" + name + "/" + age + "/" + addr);
+			}
 			
 		}
 		catch (SQLException e) {
@@ -25,6 +41,10 @@ public class DataControl {
 		}
 		finally {
 			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+				
 				if (conn != null) {
 					conn.close();
 				}
